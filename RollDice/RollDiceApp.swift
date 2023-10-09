caaf5cdd9b8c014b690a456f58d42c18d0d06b5f
Eugene Cross <eugene.cross@gmail.com>
2023-10-09 10:10:22 +0100

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Eugene on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
